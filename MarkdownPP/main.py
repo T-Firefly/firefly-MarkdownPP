64816dc56dc88d5ec6db78bfc2f0f6e5595cac68
huangjc <service@t-firefly.com>
2020-09-15 09:59:42 +0800

    MarkdownPP/main.py: 将原本的2次渲染1次展开 改成 3次渲染2次展开

--- MarkdownPP/main.py
+++ MarkdownPP/main.py
@@ -119,7 +119,7 @@ def main():
         env_dict = {}
         if args.file:
             for files in args.file:
-                env_f = open(files, 'r') 
+                env_f = open(files, 'r')
                 env_dict.update(json.load(env_f))
                 env_f.close()
 
@@ -132,7 +132,7 @@ def main():
             path = args.include
         else:
             path = []
-        
+
         modules = list(MarkdownPP.modules)
 
         if args.exclude:
@@ -141,7 +141,30 @@ def main():
                     modules.remove(module)
                 else:
                     print('Cannot exclude ', module, ' - no such module')
-                    
+
+        #1.先将.mdpp文件用jinja2渲染后的内容存到到临时文件temp中
+        # if md != sys.stdout:
+        #     Env = Environment(loader = FileSystemLoader(searchpath="./"), undefined=StrictUndefined)
+        #     template = Env.get_template(args.FILENAME)
+        #     temp = open("temp", 'w')
+        #     temp.write(template.render(env_dict))
+        #     temp.close()
+        #     mdpp.close()
+        #     mdpp = open("temp", 'r')
+
+        # #2.将temp文件用MarkdownPP生成.md文件
+        # MarkdownPP.MarkdownPP(input=mdpp, output=md, modules=modules, path=path)
+        # md.close()
+        # mdpp.close()
+
+        # #3.将.md文件再用jinja2渲染一次
+        # if md != sys.stdout:
+        #     Env = Environment(loader = FileSystemLoader(searchpath="./"), undefined=StrictUndefined)
+        #     template = Env.get_template(args.output)
+        #     md = open(args.output, 'w')
+        #     md.write(template.render(env_dict))
+        #     md.close()
+
         #1.先将.mdpp文件用jinja2渲染后的内容存到到临时文件temp中
         if md != sys.stdout:
             Env = Environment(loader = FileSystemLoader(searchpath="./"), undefined=StrictUndefined)
@@ -161,6 +184,21 @@ def main():
         if md != sys.stdout:
             Env = Environment(loader = FileSystemLoader(searchpath="./"), undefined=StrictUndefined)
             template = Env.get_template(args.output)
+            temp = open("temp", 'w')
+            temp.write(template.render(env_dict))
+            temp.close()
+            mdpp.close()
+            mdpp = open("temp", 'r')
+
+        #4.将temp文件用MarkdownPP生成.md文件
+        MarkdownPP.MarkdownPP(input=mdpp, output=md, modules=modules, path=path)
+        md.close()
+        mdpp.close()
+
+        #5.将.md文件再用jinja2渲染一次
+        if md != sys.stdout:
+            Env = Environment(loader = FileSystemLoader(searchpath="./"), undefined=StrictUndefined)
+            template = Env.get_template(args.output)
             md = open(args.output, 'w')
             md.write(template.render(env_dict))
             md.close()
@@ -170,4 +208,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-    
+
